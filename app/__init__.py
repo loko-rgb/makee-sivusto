cd61b2301bc2789bbd9be929865992231ed57bf9
luka <luka.kopeykin@gmail.com>
2025-11-03 08:41:33 +0200

    moi

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ def create_app():
         "SECRET_KEY" : "kjdvlsdk jo0439w jI)OY/)8TY(/gf hq87w0Å'ÅW'EÅRG WJUE9RHG9HE9RHG 379WY0P jqwq9f0pfhQF8G78Q03 G8 hi ag ayigsai8äwer'tprhuå6ä780rjkkä'r6k708ipodrijwhs)",
         "SESSION_TYPE" : "cachelib",
         "SESSION_SERIALIZATION_FORMAT" : 'json',
-        "SESSION_CACHELIB" : FileSystemCache(threshold=500, cache_dir=os.path.join(os.getcwd(), 'session')),
+        "SESSION_CACHELIB" : FileSystemCache(threshold=500, cache_dir= 'session'),
         "SESSION_CLEANUP_N_REQUEST" : 3
         })
     Session(app)
